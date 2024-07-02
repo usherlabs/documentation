eeac9c7dbe2d9b259a13dbfde4aa111fcd67e338
Ryan Soury <ryan@webdoodle.com.au>
2024-07-03 01:40:35 +1000

    boilerplate for creating a more versatile docs for usherlabs. attempt to upgrade to eslint 9.6

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,12 +8,12 @@ const {
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-	title: 'Log Store Docs',
+	title: 'Usher Labs Docs',
 	tagline:
-		'The Log Store Network is a research & development initiative to enable Data Protocols - Custom and verifiable data-driven digital asset management.', // INITIAL TODO
+		'Secure Web3 with trusted data: Usher Labs is an R&D firm enhancing user trust in data and digital asset management.',
 	favicon: 'img/favicon.ico',
 
-	url: 'https://docs.logstore.usher.so',
+	url: 'https://docs.usher.so',
 	// Set the /<baseUrl>/ pathname under which your site is served
 	// For GitHub pages deployment, it is often '/<projectName>/'
 	baseUrl: '/',
@@ -21,7 +21,6 @@ const config = {
 	// GitHub pages deployment config.
 	// If you aren't using GitHub pages, you don't need these.
 	organizationName: 'usherlabs',
-	projectName: 'logstore',
 	deploymentBranch: 'master',
 
 	onBrokenLinks: 'throw',
@@ -42,18 +41,34 @@ const config = {
 		[
 			'@docusaurus/plugin-content-docs',
 			{
-				routeBasePath: '/',
+				id: 'logstore',
+				path: 'docs/logstore',
+				routeBasePath: '/logstore',
 				sidebarPath: require.resolve('./sidebars.js'),
 				// Please change this to your repo.
 				// Remove this to remove the "edit this page" links.
 				editUrl:
 					// INITIAL TODO change this to your repo. This is where a user will be redirected to if they follow the link on "edit this page"
-					'https://github.com/usherlabs/logsn-docs/tree/master/',
+					'https://github.com/usherlabs/docs/tree/master/',
 			},
 		],
+		// [
+		// 	'@docusaurus/plugin-content-docs',
+		// 	{
+		// 		id: 'verity',
+		// 		path: 'docs/verity',
+		// 		routeBasePath: '/verity',
+		// 		sidebarPath: require.resolve('./sidebars.js'),
+		// 		// Please change this to your repo.
+		// 		// Remove this to remove the "edit this page" links.
+		// 		editUrl:
+		// 			// INITIAL TODO change this to your repo. This is where a user will be redirected to if they follow the link on "edit this page"
+		// 			'https://github.com/usherlabs/docs/tree/master/',
+		// 	},
+		// ],
 		// This plugin enables tailwind
 		// async function myPlugin(context, options) {
-		async function myPlugin() {
+		async function cssPlugin() {
 			return {
 				name: 'docusaurus-tailwindcss',
 				configurePostCss(postcssOptions) {
@@ -65,29 +80,29 @@ const config = {
 			};
 		},
 
-		[
-			'@docusaurus/plugin-client-redirects',
-			{
-				redirects: [
-					{
-						from: ['/network/overview/paper', '/network/overview/papers'],
-						to: '/papers',
-					},
-					{
-						from: ['/network/overview/lightpaper'],
-						to: '/papers/logstore/lightpaper',
-					},
-					{
-						from: ['/network/overview/use-cases', '/use-cases'],
-						to: '/papers/logstore/use-cases',
-					},
-					{
-						from: ['/network/sdk/client-side-network-verification'],
-						to: '/network/sdk/network-verification',
-					},
-				],
-			},
-		],
+		// [
+		// 	'@docusaurus/plugin-client-redirects',
+		// 	{
+		// 		redirects: [
+		// 			{
+		// 				from: ['/network/overview/paper', '/network/overview/papers'],
+		// 				to: '/papers',
+		// 			},
+		// 			{
+		// 				from: ['/network/overview/lightpaper'],
+		// 				to: '/papers/logstore/lightpaper',
+		// 			},
+		// 			{
+		// 				from: ['/network/overview/use-cases', '/use-cases'],
+		// 				to: '/papers/logstore/use-cases',
+		// 			},
+		// 			{
+		// 				from: ['/network/sdk/client-side-network-verification'],
+		// 				to: '/network/sdk/network-verification',
+		// 			},
+		// 		],
+		// 	},
+		// ],
 	],
 	// algolia: { // INITIAL TODO to activate algolia search. Fill according to your needs
 	//     appId: '',
@@ -119,8 +134,8 @@ const config = {
 				// Plugin Options for loading OpenAPI files
 				specs: [
 					{
-						spec: './docs/network/api/_api.spec.yaml',
-						route: '/network/api/reference',
+						spec: './docs/logstore/node/api/_api.spec.yaml',
+						route: '/logstore/node/api/reference',
 					},
 				],
 				// Theme Options for modifying how redoc renders them
@@ -135,13 +150,12 @@ const config = {
 	themeConfig:
 		/** @type {import('@docusaurus/preset-classic').ThemeConfig} */
 		({
-			// image: "img/docusaurus-social-card.jpg",
 			navbar: {
 				hideOnScroll: true,
 				logo: {
-					alt: 'Site Logo', // no need to change, it's the default for accessibility reasons
-					src: '/logos/logstore-logo.png',
-					srcDark: '/logos/logstore-logo-white.png', // INITIAL TODO
+					alt: 'Usher Labs Logo',
+					src: '/img/usherlabs/Logo-Text.png',
+					srcDark: '/img/usherlabs/Logo-Text-White-500.png',
 					target: '_self',
 					width: '130',
 					href: '/',
@@ -149,21 +163,37 @@ const config = {
 				items: [
 					{
 						type: 'docSidebar',
-						sidebarId: 'network',
-						label: 'Network',
-						position: 'left',
-					},
-					{
-						type: 'docSidebar',
-						sidebarId: 'node',
-						label: 'Node',
-						position: 'left',
-					},
-					{
-						type: 'docSidebar',
 						sidebarId: 'papers',
 						label: 'Papers',
-						position: 'left',
+						position: 'right',
+					},
+					{
+						type: 'dropdown',
+						label: 'Log Store',
+						position: 'right',
+						items: [
+							{
+								type: 'docSidebar',
+								sidebarId: 'network',
+								label: 'Network',
+							},
+							{
+								type: 'docSidebar',
+								sidebarId: 'node',
+								label: 'Node',
+							},
+							{
+								type: 'docSidebar',
+								sidebarId: 'papers',
+								label: 'Papers',
+							},
+						],
+					},
+					{
+						type: 'docSidebar',
+						sidebarId: 'verity',
+						label: 'Verity',
+						position: 'right',
 					},
 					// Right side starts here
 					{
@@ -174,26 +204,6 @@ const config = {
 						label: 'Stay tuned',
 						position: 'right',
 						items: [
-							// 					{
-							// 						type: "custom-iconLink",
-							// 						position: "right",
-							// 						icon: {
-							// 							alt: "twitter logo",
-							// 							src: `/logos/twitter.svg`,
-							// 							href: "https://twitter.com/usher_web3",
-							// 							target: "_blank"
-							// 						}
-							// 					},
-							// 					{
-							// 						type: "custom-iconLink",
-							// 						position: "right",
-							// 						icon: {
-							// 							alt: "discord logo",
-							// 							src: `/logos/discord.svg`,
-							// 							href: "https://go.usher.so/discord",
-							// 							target: "_blank"
-							// 						}
-							// 					},
 							{
 								href: 'https://logstore.usher.so',
 								label: 'Our Homepage',
@@ -220,14 +230,6 @@ const config = {
 							},
 						],
 					},
-					// {
-					// 	// INITIAL TODO may change if you would want different action from user
-					// 	type: 'custom-cta',
-					// 	position: 'right',
-					// 	label: 'Return to Website',
-					// 	target: '_blank',
-					// 	href: 'https://logstore.usher.so',
-					// },
 					{
 						type: 'custom-separator',
 						position: 'right',
@@ -237,7 +239,7 @@ const config = {
 						position: 'right',
 						icon: {
 							alt: 'github logo',
-							src: `/logos/github.svg`,
+							src: `/img/github.svg`,
 							href: 'https://github.com/usherlabs/logstore',
 							target: '_blank',
 						},
